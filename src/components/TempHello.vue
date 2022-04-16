6b6b7e43eb32ed49e708e26c0f4ed45903ae7b49
Cheng_DX <2337040896@qq.com>
2022-04-16 17:40:03 +0800

    feat: add github icon

--- src/components/TempHello.vue
+++ src/components/TempHello.vue
@@ -5,18 +5,22 @@ import HandleIcon from './icons/HandleIcon.vue'
 import PixelIcon from './icons/PixelIcon.vue'
 import FormulaIcon from './icons/FormulaIcon.vue'
 import ProjectItem from './ProjectItem.vue'
+import GitHubIcon from './icons/GitHubIcon.vue'
 
 const projects = ref([
   {
     path: 'handle',
+    name: '汉兜',
     icon: HandleIcon
   },
   {
     path: 'pixel-box',
+    name: '像素盒子',
     icon: PixelIcon
   },
   {
     path: 'formula-editor',
+    name: '公式编辑器',
     icon: FormulaIcon
   }
 ])
@@ -27,10 +31,13 @@ const projects = ref([
     <building-icon />
     <span class="content">建设中</span>
     <div class="projects">
-      <project-item v-for="p in projects" :path="p.path">
+      <project-item v-for="p in projects" :path="p.path" :title="p.name">
         <component :is="p.icon" class="project-icon" />
       </project-item>
     </div>
+    <a href="https://github.com/Cheng-DX">
+      <git-hub-icon class="github-icon" />
+    </a>
   </div>
 </template>
 
@@ -58,5 +65,11 @@ const projects = ref([
   width: 30px;
   height: 30px;
   margin-inline: 5px;
+}
+.github-icon {
+  width: 20px;
+  height: 20px;
+  margin-top: 10px;
+  cursor: pointer;
 }
 </style>
